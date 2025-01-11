7be8ad4dca3e61f4e46ebd0b43c2d204c3d38c8c
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-11 21:32:23 +0500

    dev only for create route

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -2,16 +2,20 @@ const express = require('express') ;
 const router = express.Router() ;
 const ownerModel = require('../models/owner.model');
 
-router.get('/',(req,res)=>{
-    res.send('owner route') ;
-})
 
-
+// create route only for development
 if(process.env.NODE_ENV === 'development'){
     router.post('/create',(req,res)=>{
         res.send('create route working') ;
     })
 } 
+
+//  default route
+router.get('/',(req,res)=>{
+    res.send('owner route') ;
+})
+
+
 // post route
 
 router.post('/create',(req,res)=>{
