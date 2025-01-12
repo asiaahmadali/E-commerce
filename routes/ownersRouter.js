6f772c8dac675567da931a3b7ef2364496559d00
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-12 20:06:39 +0500

    owner validity testing

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -11,6 +11,7 @@ if(process.env.NODE_ENV === 'development'){
         if(owners.length > 0) return res.status(503).send('no permission for new owner') ;
 
         const {fullname, email, password} = req.body ;
+        // make owner
         const createdowner = ownerModel.create({
               fullname,
               email,
