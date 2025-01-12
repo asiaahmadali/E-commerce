79dc28ce45f237033d114a12d7ac626dc4284713
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-12 20:05:32 +0500

    debugger setup

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose') ;
 const config = require('config') ;
 const dbgr = require('debug')('development:mongoose') ;
 
-
+// debugger setup
 mongoose.connect(`${config.get("MONGODB_URI")}/miniBagShop`).then(()=>{
    dbgr('connected') ;
 }).catch((err)=>{
