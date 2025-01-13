50e4b4edc7ac7c9aa2f643138c23c66984798b6e
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2025-01-13 22:34:22 +0500

    production disable

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -3,7 +3,7 @@ const router = express.Router() ;
 const ownerModel = require('../models/owner.model');
 
 
-// create route only for development mode 
+// create route only for development mode (prevent production)
 if(process.env.NODE_ENV === 'development'){
     router.post('/create',async (req,res)=>{
         const owners = await ownerModel.find();
